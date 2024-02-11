fa343e6a51d45a1629a091d98db2d05d66cb06e6
Bimbo <88099229+BimboXl@users.noreply.github.com>
2024-02-10 22:00:12 -0600

    Fixed Freeze Time and Time Sync

--- resources/[Bimbo]/effects/client/time.lua
+++ resources/[Bimbo]/effects/client/time.lua
@@ -1,5 +1,8 @@
 local kvpValue = GetResourceKvpString("graphics_time")
-local baseTime = 12
+local baseTime = 900
+local timeOffset = 0
+local timer = 0
+local freezeTime = false
 local currentTime = kvpValue or baseTime
 
 AddEventHandler('erotic:playerSpawned', function()
@@ -8,10 +11,9 @@ AddEventHandler('erotic:playerSpawned', function()
 end)
 
 RegisterCommand("time", function(source, args)
-    local hour = tonumber(args[1])
+    local hour = (tonumber(args[1]) * 100)
 
     currentTime = hour
-
     SetGameTime(currentTime)
 end, false)
 
@@ -19,3 +21,39 @@ function SetGameTime(currentTime)
     NetworkOverrideClockTime(currentTime, 0, 0)
     SetResourceKvp("graphics_time", currentTime)
 end
+
+RegisterNUICallback('getCurrentTimeSettings', function(data, cb)
+    cb(GetResourceKvpString("graphics_time") or "12")
+end)
+
+RegisterNUICallback('getTimeSettings', function(data, cb)
+    local timeSettings = {}
+    for i = 0, 24 do
+        table.insert(timeSettings, i)
+    end
+    print("Sending time settings to client:", json.encode(timeSettings))
+    cb(timeSettings)
+end)
+
+RegisterCommand("freezetime", function(source, args)
+    freezeTime = not freezeTime
+end, false)
+
+CreateThread(function()
+    while true do
+        Wait(0)
+        local newBaseTime = currentTime
+        if GetGameTimer() - 500 > timer then
+          newBaseTime = newBaseTime + 0.25
+          timer = GetGameTimer()
+        end
+        if freezeTime then
+          timeOffset = timeOffset + currentTime - newBaseTime
+        end
+        currentTime = newBaseTime
+
+        hour = math.floor(((currentTime + timeOffset) / 60) % 24)
+        -- minute = math.floor((currentTime + timeOffset) % 60)
+        NetworkOverrideClockTime(hour, 0, 0)
+    end
+end)
