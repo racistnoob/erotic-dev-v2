80732a3da031d555c53555da9a2f301f929e168c
Bimbo <88099229+BimboXl@users.noreply.github.com>
2024-02-12 01:30:01 -0600

    Updated Visibility for Effects

--- resources/[Bimbo]/effects/web/src/providers/VisibilityProvider.tsx
+++ resources/[Bimbo]/effects/web/src/providers/VisibilityProvider.tsx
@@ -31,7 +31,7 @@ export const VisibilityProvider: React.FC<{ children: React.ReactNode }> = ({
     if (!visible) return;
 
     const keyHandler = (e: KeyboardEvent) => {
-      if (["Backspace", "Escape"].includes(e.code)) {
+      if (["Escape"].includes(e.code)) {
         if (!isEnvBrowser()) fetchNui("hideFrame");
         else setVisible(!visible);
       }
@@ -50,7 +50,12 @@ export const VisibilityProvider: React.FC<{ children: React.ReactNode }> = ({
       }}
     >
       <div
-        style={{ visibility: visible ? "visible" : "hidden", height: "100%" }}
+        style={{
+          visibility: visible ? 'visible' : 'hidden',
+          opacity: visible ? 1 : 0,
+          transition: 'opacity 0.5s ease-in-out',
+          height: '100%',
+        }}
       >
         {children}
       </div>
