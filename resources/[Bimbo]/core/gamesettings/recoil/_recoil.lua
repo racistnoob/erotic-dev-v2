36022186ae35593117e878285fcb8c43ecb08ed8
noob <65266187+racistnoob@users.noreply.github.com>
2024-02-15 08:06:30 +0000

    plug

--- resources/[Bimbo]/core/gamesettings/recoil/_recoil.lua
+++ resources/[Bimbo]/core/gamesettings/recoil/_recoil.lua
@@ -11,7 +11,7 @@ function Recoil:SetMode(mode)
     if (GetConvarInt('debugger_recoil', 0) == 1) then
         TriggerEvent("arena:postNotify", "Set recoil to mode '" .. tostring(mode) .. "'.", Config.UI.successColour)
     end]]
-
+    
     for k, v in pairs(self.onResetCallbacks) do
         v(mode)
     end
@@ -106,40 +106,40 @@ local recoilModes = {
         }
     },
 }
-
--- RegisterCommand("recoil", function(source, args, rawCommand)
---     local player = source
-
---     if #args < 1 then
---         return
---     end
-
---     local mode = args[1]:lower()
-
---     if not recoilModes[mode] then
---         local modesList = table.concat(table.keys(recoilModes), ", ")
---         return
---     end
-
---     Recoil:SetMode(mode)
---     print("^2Requested mode: " .. mode)
-
---     local commands = recoilModes[mode].commands
-
---     for _, command in ipairs(commands) do
-
---     end
--- end)
+--[[
+RegisterCommand("recoil", function(source, args, rawCommand)
+    local player = source
+    
+    if #args < 1 then
+        return
+    end
+    
+    local mode = args[1]:lower()
+    
+    if not recoilModes[mode] then
+        local modesList = table.concat(table.keys(recoilModes), ", ")
+        return
+    end
+    
+    Recoil:SetMode(mode)
+    print("^2Requested mode: " .. mode)
+    
+    local commands = recoilModes[mode].commands
+    
+    for _, command in ipairs(commands) do
+        
+    end
+end)]]
 
 function SetRecoilMode(player, mode)
     mode = mode:lower()
-
+    
     if not recoilModes[mode] then
         local modesList = table.concat(table.keys(recoilModes), ", ")
         --print("^1Invalid mode. Available modes: " .. modesList)
         return
     end
-
+    
     Recoil:SetMode(mode)
     --print("^2Recoil set: " .. mode)
 end
